504648011ec36995675e0db04d921e4a27427907
Astafieva <olia040889@gmail.com>
2018-10-29 12:05:35 +0300

    Lesson 04 A1, A2,A3 done

--- src/by/it/astafieva/lesson04/TaskA3.java
+++ src/by/it/astafieva/lesson04/TaskA3.java
@@ -22,6 +22,25 @@ package by.it.astafieva.lesson04;
  */
 
 
-public class TaskA3 {
+import java.util.Scanner;
 
+public class TaskA3
+{
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+
+            if(a==b && a!=c)
+            System.out.println(a + " "+ b);
+            else if(c==b && c != a)
+            System.out.println(c + " "+ b);
+            else if(a==c && a != b)
+            System.out.println(a + " "+ c);
+            else if(a==b && a==c)
+            System.out.println(a + " "+ b + " " + c);
+            else
+            System.out.println("Среди чисел нет равных");
+        }
 }
