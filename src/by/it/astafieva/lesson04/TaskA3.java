21abfabae00518808bb6963e56717eb948fae323
Astafieva <olia040889@gmail.com>
2018-11-02 15:30:07 +0300

    Lesson 05 A1, A2 done

--- src/by/it/astafieva/lesson04/TaskA3.java
+++ src/by/it/astafieva/lesson04/TaskA3.java
@@ -27,8 +27,8 @@ import java.util.Scanner;
 public class TaskA3
 {
         public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            int a = sc.nextInt();
+                Scanner sc = new Scanner(System.in);
+                int a = sc.nextInt();
             int b = sc.nextInt();
             int c = sc.nextInt();
 
