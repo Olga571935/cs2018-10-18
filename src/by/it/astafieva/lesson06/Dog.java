40b101d3e7d531ec176090872ae58f99c5fc49ee
akoltovich <alexeyugol@gmail.com>
2018-11-03 11:54:11 +0300

    Lesson 6: A1, A2 done

--- src/by/it/astafieva/lesson06/Dog.java
+++ src/by/it/astafieva/lesson06/Dog.java
@@ -1,8 +1,17 @@
 package by.it.astafieva.lesson06;
 
 public class Dog {
+
     private String name;
     private int age;
+
+    public Dog() {
+    }
+
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 
 
     public String getName() {
@@ -19,5 +28,12 @@ public class Dog {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Кличка: " + name + ". Возраст: " + age;
+
+
     }
 }
